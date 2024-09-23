23477c88717d24e09379701fca0c4a2368519c2f
Ashraful Abedin Fardin <95529786+AshrafulAbedin@users.noreply.github.com>
2024-09-23 22:17:01 +0600

    Update 7.c

--- 05/projects/07/7.c
+++ 05/projects/07/7.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
 
-    int n1, n2, n3, n4, max, min, min1, min2;
+    int n1, n2, n3, n4, max, min, max1, max2, min1, min2;
 
     printf("Enter four integers: ");
     scanf("%d%d%d%d", &n1, &n2, &n3, &n4);
 
-    if (n1 >= n2) {
+    if (n1 >= n2)
+    {
         max1 = n1;
         min1 = n2;
-    } else { 
+    }
+    else
+    {
         max1 = n2;
         min1 = n1;
     }
-    
-    if (n3 >= n4) {
+
+    if (n3 >= n4)
+    {
         max2 = n3;
         min2 = n4;
-    } else {
+    }
+    else
+    {
         max2 = n4;
         min2 = n3;
     }
